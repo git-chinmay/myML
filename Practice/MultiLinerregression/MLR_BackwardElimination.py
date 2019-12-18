b3f3bdb8140a23623861e7808ccba93499901972
Chinmay <nayak.chinmay06@gmail.com>
2019-12-18 12:05:49 +0530

    Backward elimination done

--- Practice/MultiLinerregression/MLR_BackwardElimination.py
+++ Practice/MultiLinerregression/MLR_BackwardElimination.py
@@ -2,7 +2,8 @@ import numpy as np
 import pandas as pd
 
 #STEP-1 DATA PreProcessing
-dataset = pd.read_csv(r"C:\Users\n0278588\GITHUB-Local\myML\Practice\MultiLinerregression\50_Startups.csv")
+#dataset = pd.read_csv(r"C:\Users\n0278588\GITHUB-Local\myML\Practice\MultiLinerregression\50_Startups.csv")
+dataset = pd.read_csv(r"E:\VSCODE\GIT_Hub\myML\Practice\MultiLinerregression\50_Startups.csv")
 
 #defining the input feature matrix and output vector
 #print(dataset.columns)
@@ -30,11 +31,13 @@ y_predict = regressor.predict(X_test)
 #print(f"Predicted dataset: {y_predict}")
 
 #STEP-3 Using backward elimination Technique to understand which feature is the important factor
-import statsmodels.formula.api as sm
+#import statsmodels.formula.api as sm
 
 #Adding an extra column of 1s 
-X = np.append(arr = np.ones(
-    (50,1)).astype(int),values=X, axis=1)
+#50 rows,1 column 
+#arr should be arr = X but it will add new column 1 to right side of X but we need it to be added as 1st column
+#Hence we reversed it and value = X now.It will add our X to column of 1s
+X = np.append(arr = np.ones((50,1)).astype(int),values=X, axis=1)
 
 #Choose a Significance level usually 0.05
 #If P>0.05 for the highest values parameter,remove that value
@@ -57,26 +60,37 @@ ols.summary()"""
 
 import statsmodels.regression.linear_model as lm
 
-#x_opt = X[:,[0,1,2,3,4,5]]
+#Creating an optimal matrices of features
+
+x_opt = X[:,[0,1,2,3,4,5]]
+
+#OLS :- Ordinary Least Square
 #regressor_ols = lm.OLS(endog = y, exog= x_opt).fit()
 #print(regressor_ols.summary())
 
-#Remove the 4th column as x4 = 0.990 in previous run
-#x_opt = X[:,[0,1,2,3,5]]
+#Remove the 4th(index 4) from  x_opt = X[:,[0,1,2,3,4,5]] column as x4 = 0.990 in previous run
+x_opt = X[:,[0,1,2,3,5]]
 #regressor_ols = lm.OLS(endog = y, exog= x_opt).fit()
 #print(regressor_ols.summary())
 
-
-#Remove the 5th column as x4 = 0.940
-#x_opt = X[:,[0,1,2,3]]
+#Remove the 5th column(Index 4) from x_opt = X[:,[0,1,2,3,5]] as x4 = 0.940
+x_opt = X[:,[0,1,2,3]]
 #regressor_ols = lm.OLS(endog = y, exog= x_opt).fit()
 #print(regressor_ols.summary())
 
+#Remove the 3rd Column(Index 2) from x_opt = X[:,[0,1,2,3]] as x2 = 0.6
+x_opt = X[:,[0,1,3]]
+#regressor_ols = lm.OLS(endog = y, exog= x_opt).fit()
+#print(regressor_ols.summary())
 
-#Remove the 1st column as x2 = 0.6
-x_opt = X[:,[0,2,3]]
+#Remove the 3rd column(index 2) from x_opt = X[:,[0,1,3]] as x2 = 0.06
+x_opt = X[:,[0,1]]
 regressor_ols = lm.OLS(endog = y, exog= x_opt).fit()
 print(regressor_ols.summary())
+
+##Verdict :- Now we have only column x0 which is column of 1s and x1 which is Original column R & D 
+#So it looks like the most impact ful column is R & D in preditcing the y
+
 
 
 
