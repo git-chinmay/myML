48d692f66fd1e6279eb8e35c0119ab5cc68520e6
Chinmay <nayak.chinmay06@gmail.com>
2019-12-26 12:16:28 +0530

    Logistic Regression visualisation added

--- Practice/LogisticRegression/LogiRgsn.py
+++ Practice/LogisticRegression/LogiRgsn.py
@@ -12,7 +12,7 @@ Observations :
 4)With confusion matrix
 Confusion matrix: [[66  2]
                   [ 9 23]]
-                  
+
 89 (66+23) correct predictions
 11 (9+2) In correct predictions
 """
@@ -22,16 +22,16 @@ import matplotlib.pyplot as plt
 
 dataset = pd.read_csv(r'E:\VSCODE\GIT_Hub\myML\Practice\LogisticRegression\Social_Network_Ads.csv')
 
-X = dataset.iloc[:,:-1].values #May need hot encoding for Gender column
-#X = dataset.iloc[:,[0,2,3]].values # By this we can select specifc columns
+#X = dataset.iloc[:,:-1].values #May need hot encoding for Gender column
+X = dataset.iloc[:,[2,3]].values # By this we can select specifc columns
 y = dataset.iloc[:,4].values
 
 #Hot encoding
-from sklearn.preprocessing import LabelEncoder,OneHotEncoder
-encoder = LabelEncoder()
-X[:,1] = encoder.fit_transform(X[:,1])
-hotencoder = OneHotEncoder()
-X= hotencoder.fit_transform(X).toarray()
+#from sklearn.preprocessing import LabelEncoder,OneHotEncoder
+#encoder = LabelEncoder()
+#X[:,1] = encoder.fit_transform(X[:,1])
+#hotencoder = OneHotEncoder()
+#X= hotencoder.fit_transform(X).toarray()
 
 
 
@@ -74,6 +74,25 @@ print(f"\nNo of wrong predictions: {match} out of total {length_of_array} observ
 from sklearn.metrics import confusion_matrix
 cm = confusion_matrix(y_test,y_pred)
 print(f"Confusion matrix: {cm}")
+
+#Visualisation
+from matplotlib.colors import ListedColormap
+#X_set, y_set = X_train, y_train
+X_set, y_set = X_test, y_test
+X1, X2 = np.meshgrid(np.arange(start = X_set[:, 0].min() - 1, stop = X_set[:, 0].max() + 1, step = 0.01),
+                     np.arange(start = X_set[:, 1].min() - 1, stop = X_set[:, 1].max() + 1, step = 0.01))
+plt.contourf(X1, X2, classifier.predict(np.array([X1.ravel(), X2.ravel()]).T).reshape(X1.shape),
+             alpha = 0.75, cmap = ListedColormap(('red', 'green')))
+plt.xlim(X1.min(), X1.max())
+plt.ylim(X2.min(), X2.max())
+for i, j in enumerate(np.unique(y_set)):
+    plt.scatter(X_set[y_set == j, 0], X_set[y_set == j, 1],
+                c = ListedColormap(('red', 'green'))(i), label = j)
+plt.title('Classifier (Test set)')
+plt.xlabel('Age')
+plt.ylabel('Estimated Salary')
+plt.legend()
+plt.show()
 
 
 
